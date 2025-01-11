0010707b7908a78fdc525633288093873c5d48d6
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-01-11 16:57:31 +0000

    HW #4 React

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import customToast from '../ErrorMessage/Toast/ToastMessage';
+import s from './SearchBar.module.css';
+
+const SearchBar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (query.trim() === '') {
+      customToast('warn', 'Oops... Enter something');
+      return;
+    }
+
+    onSubmit(query);
+    setQuery('');
+  };
+  return (
+    <header className={s.header}>
+      <form onSubmit={handleSubmit} className={s.searchForm}>
+        <input
+          type="text"
+          name="query"
+          className={s.searchInput}
+          placeholder="Search images and photos"
+          value={query}
+          onChange={e => setQuery(e.target.value.toLowerCase())}
+        />
+        <button type="submit" className={s.searchBtn}>
+          Search
+        </button>
+      </form>
+    </header>
+  );
+};
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export default SearchBar;
